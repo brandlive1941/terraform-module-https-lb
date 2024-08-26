88fcdbbf80440bc53ba17030539736911fbf53b1
drewmercer-bl <drew.mercer@brandlive.com>
2024-08-26 15:54:06 -0700

    update fix?

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "buckets" {
 # Load Balancer
 module "lb" {
   source                = "terraform-google-modules/lb-http/google//modules/serverless_negs"
-  version               = "~> 10.0"
+  version               = "~> 11.1.0"
   project               = var.project_id
   name                  = local.load_balancer_name
   load_balancing_scheme = "EXTERNAL_MANAGED"
