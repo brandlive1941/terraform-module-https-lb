70b005c1efeb7fd7a2d9c028e75914d58bb49686
drewmercer-bl <drew.mercer@brandlive.com>
2024-10-28 09:43:58 -0700

    fixing?

--- main.tf
+++ main.tf
@@ -136,19 +136,16 @@ resource "google_compute_url_map" "urlmap" {
           }
         }
       }
-      dynamic "default_custom_error_response_policy" {
-        for_each = path_matcher.value.backend["default_custom_error_response_policy"]
-        content {
-          dynamic "error_response_rule" {
-            for_each = default_custom_error_response_policy.value.error_response_rule
-            content {
-              match_response_codes   = error_response_rule.value.match_response_codes
-              path                   = error_response_rule.value.path
-              override_response_code = error_response_rule.value.override_response_code
-            }
+      default_custom_error_response_policy {
+        dynamic "error_response_rule" {
+          for_each = default_custom_error_response_policy.value.error_response_rule
+          content {
+            match_response_codes   = error_response_rule.value.match_response_codes
+            path                   = error_response_rule.value.path
+            override_response_code = error_response_rule.value.override_response_code
           }
-          error_service = default_custom_error_response_policy.value.error_service
         }
+        error_service = default_custom_error_response_policy.value.error_service
       }
     }
   }
