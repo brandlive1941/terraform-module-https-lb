a6b186e4c7d519ad6469a7680bd2807d2411592f
Christopher Goodwin <christophergoodwin@ChristodwinsMBP.localdomain>
2025-01-09 15:28:17 -0800

    bugfix/bdev-437/bssrf

--- main.tf
+++ main.tf
@@ -57,6 +57,7 @@ module "buckets" {
   source                               = "github.com/brandlive1941/terraform-module-backend-bucket?ref=v1.1.0"
   project_id                           = var.project_id
   name                                 = each.value["name"]
+  environment                          = each.value["environment"]
   location                             = each.value["location"]
   service_name                         = each.value["service_name"]
   enable_cdn                           = each.value.backend["enable_cdn"]
