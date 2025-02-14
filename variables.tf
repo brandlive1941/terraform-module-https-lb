bbfe890763b4547d5c7ae06d44a3db1fc011b3d5
drewmercer-bl <drew.mercer@brandlive.com>
2025-02-14 13:22:45 -0800

    fix(main.tf): fixing empty changes

--- variables.tf
+++ variables.tf
@@ -173,7 +173,7 @@ variable "services" {
           override_response_code = optional(number)
         })))
         error_service = optional(string)
-      }), {})
+      }))
       iap_config = optional(object({
         enable               = bool
         oauth2_client_id     = optional(string)
